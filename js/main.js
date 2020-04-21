b6f6d1f2134da714d894d638bee0f1a20cb7db4e
DeLettenhove <deletten@gmail.com>
2020-04-21 23:54:23 +0300

    StarWars v1.0

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(".owl-carousel").owlCarousel({
+  margin: 10,
+  nav: true,
+  navClass: ["slider__item--left", "slider__item--right"],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    900: {
+      items: 3,
+    },
+    1200: {
+      items: 4,
+    },
+  },
+});
